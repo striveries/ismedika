c26d35ce40655eeb25838d49a3beda1c61b7c66a
striveries <121800213+striveries@users.noreply.github.com>
2024-10-08 01:59:39 +0700

    Initial commit

---
+++ src/main/java/apap/ti/hospitalization2206082064/Hospitalization2206082064Application.java
@@ -0,0 +1,13 @@
+package apap.ti.hospitalization2206082064;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hospitalization2206082064Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hospitalization2206082064Application.class, args);
+	}
+
+}
---
+++ src/test/java/apap/ti/hospitalization2206082064/Hospitalization2206082064ApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti.hospitalization2206082064;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hospitalization2206082064ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
